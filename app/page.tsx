89aa457bbc5b91b91b184ccc02c6160f7a38f07f
jacobk999 <jababakoshy@gmail.com>
2024-04-09 19:38:51 -0400

    feat: initial commit with homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,242 @@
-import Image from "next/image";
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { LoginIcon } from "~/components/icons/login";
+import { PlusIcon } from "~/components/icons/plus";
+import { Button } from "~/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "~/components/ui/card";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [tab, setTab] = useState("create");
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	return (
+		<Tabs defaultValue="create" className="w-[500px]" onValueChange={setTab}>
+			<TabsList className="w-full">
+				<TabsTrigger value="create" className="grow gap-1">
+					<PlusIcon filled={tab === "create"} />
+					<p>Create</p>
+				</TabsTrigger>
+				<TabsTrigger value="join" className="grow gap-1">
+					<LoginIcon filled={tab === "join"} />
+					<p>Join</p>
+				</TabsTrigger>
+			</TabsList>
+			<TabsContent value="create">
+				<CreateGameCard />
+			</TabsContent>
+			<TabsContent value="join">
+				<JoinGameCard />
+			</TabsContent>
+		</Tabs>
+	);
+}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+const SIZE_DEFAULT = 5;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+const RoomNameSchema = z.string().min(3).max(16);
+const UsernameSchema = z.string().min(3).max(16);
+const SizeSchema = z.coerce.number().min(3).max(11);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+const CreateGameSchema = z.object({
+	roomName: RoomNameSchema,
+	username: UsernameSchema,
+	width: SizeSchema,
+	height: SizeSchema,
+});
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+function CreateGameCard() {
+	const form = useForm<z.infer<typeof CreateGameSchema>>({
+		resolver: zodResolver(CreateGameSchema),
+		defaultValues: {
+			width: SIZE_DEFAULT,
+			height: SIZE_DEFAULT,
+		},
+	});
+
+	const width = form.watch("width");
+	const height = form.watch("height");
+
+	const router = useRouter();
+
+	async function onSubmit(values: z.infer<typeof CreateGameSchema>) {
+		alert(JSON.stringify(values, null, 2));
+		router.push("/room");
+	}
+
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Create a New Game</CardTitle>
+			</CardHeader>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)}>
+					<CardContent className="space-y-2">
+						<FormField
+							control={form.control}
+							name="roomName"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Room Name</FormLabel>
+									<FormControl>
+										<Input {...field} />
+									</FormControl>
+									<FormDescription>The room name</FormDescription>
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="username"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Username</FormLabel>
+									<FormControl>
+										<Input {...field} />
+									</FormControl>
+									<FormDescription>
+										Your public display name ({UsernameSchema.minLength} -
+										{UsernameSchema.maxLength} characters)
+									</FormDescription>
+								</FormItem>
+							)}
+						/>
+						<div className="flex gap-2">
+							<FormField
+								control={form.control}
+								name="width"
+								render={({ field }) => (
+									<FormItem className="grow">
+										<FormLabel>Width</FormLabel>
+										<FormControl>
+											<Input
+												{...field}
+												type="number"
+												min={SizeSchema.minValue!}
+												max={SizeSchema.maxValue!}
+											/>
+										</FormControl>
+										<FormDescription>
+											The width of the board ({SizeSchema.minValue}-
+											{SizeSchema.maxValue})
+										</FormDescription>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="height"
+								render={({ field }) => (
+									<FormItem className="grow">
+										<FormLabel>Height</FormLabel>
+										<FormControl>
+											<Input
+												{...field}
+												type="number"
+												min={SizeSchema.minValue!}
+												max={SizeSchema.maxValue!}
+											/>
+										</FormControl>
+										<FormDescription>
+											The height of the board ({SizeSchema.minValue}-
+											{SizeSchema.maxValue})
+										</FormDescription>
+									</FormItem>
+								)}
+							/>
+						</div>
+					</CardContent>
+					<CardFooter>
+						<Button className="w-full" type="submit">
+							Create Game
+						</Button>
+					</CardFooter>
+				</form>
+			</Form>
+		</Card>
+	);
+}
+const JoinGameSchema = z.object({
+	roomName: RoomNameSchema,
+	username: UsernameSchema,
+});
+
+function JoinGameCard() {
+	const form = useForm<z.infer<typeof JoinGameSchema>>({
+		resolver: zodResolver(JoinGameSchema),
+	});
+
+	const router = useRouter();
+
+	async function onSubmit(values: z.infer<typeof JoinGameSchema>) {
+		alert(JSON.stringify(values, null, 2));
+		router.push("/room");
+	}
+
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Join an Existing Game</CardTitle>
+			</CardHeader>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)}>
+					<CardContent className="space-y-2">
+						<FormField
+							control={form.control}
+							name="roomName"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Room Name</FormLabel>
+									<FormControl>
+										<Input {...field} />
+									</FormControl>
+									<FormDescription>The room name</FormDescription>
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="username"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Username</FormLabel>
+									<FormControl>
+										<Input {...field} />
+									</FormControl>
+									<FormDescription>
+										Your public display name ({UsernameSchema.minLength} -
+										{UsernameSchema.maxLength} characters)
+									</FormDescription>
+								</FormItem>
+							)}
+						/>
+					</CardContent>
+					<CardFooter>
+						<Button className="w-full" type="submit">
+							Join Game
+						</Button>
+					</CardFooter>
+				</form>
+			</Form>
+		</Card>
+	);
 }
